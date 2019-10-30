795c175e5d9088e5559a2f24b4025a4fc24450af
HrabiaVulpes <hrabiavulpes@gmail.com>
2019-10-30 23:08:29 +0100

    Basis for GUI

---
+++ src/main/java/game/gui/BaseGUI.java
@@ -0,0 +1,29 @@
+package game.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BaseGUI extends JFrame {
+
+    public BaseGUI() {
+        initUI();
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new BaseGUI();
+            ex.setVisible(true);
+        });
+    }
+
+    private void initUI() {
+        JPanel drawPanel = new MapPanel();
+        add(drawPanel);
+
+        setSize(350, 250);
+        setTitle("Points");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
