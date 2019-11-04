64f158132ca928f0b77853b35b33c8783bcd61a7
Grzegorz Mucha <Grzegorz.Mucha@ig.com>
2019-11-04 16:36:50 +0100

    basics of  control

--- src/main/java/game/CommonData.java
+++ src/main/java/game/CommonData.java
@@ -1,6 +1,8 @@
 package game;
 
 import game.player.Player;
+import game.player.diplomacy.Message;
+import game.player.government.Order;
 
 import java.util.Set;
 
@@ -9,4 +11,6 @@ public class CommonData {
    public static Long gameSpeed = 10000L;                 // miliseconds per turn
    public static Set<Player> players;
    public static Set<System> galaxy;
+   public static Set<Message> messages;
+   public static Set<Order> orders;
 }
--- src/main/java/game/player/government/Order.java
+++ src/main/java/game/player/government/Order.java
@@ -1,5 +1,12 @@
 package game.player.government;
 
+import game.map.Ship;
+
 public class Order {
    OrderType type;
+
+   Ship receivingShip;
+
+   System targetSystem;
+   Ship targetShip;
 }
