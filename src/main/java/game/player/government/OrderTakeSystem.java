f0451939e626c69b332e73a26fc48389acd945b7
Grzegorz Mucha <Grzegorz.Mucha@ig.com>
2019-11-06 12:09:36 +0100

    fix to system taking

--- src/main/java/game/player/government/OrderTakeSystem.java
+++ src/main/java/game/player/government/OrderTakeSystem.java
@@ -7,11 +7,13 @@ import game.player.Player;
 import game.ship.Ship;
 
 import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class MoveTakeSystem extends Order {
+public class OrderTakeSystem extends Order {
    private String shipID;
 
-   public MoveTakeSystem(Player owner, String shipID) {
+   public OrderTakeSystem(Player owner, String shipID) {
       super(owner, OrderType.SHIP_TAKE_SYSTEM);
       this.shipID = shipID;
    }
@@ -28,7 +30,12 @@ public class MoveTakeSystem extends Order {
               .filter(fortress -> fortress.whereIsFortress == system)
               .findFirst().orElse(new Fortress("none", system));
 
-      if (systemFortress.fortifiedLevel <= 0) {
+      Set<Ship> foreignShipsPresent = CommonData.ships.stream()
+              .filter(ship -> ship.whereIsShip == system)
+              .filter(ship -> !ship.owner.equals(owner.name))
+              .collect(Collectors.toSet());
+
+      if (systemFortress.fortifiedLevel <= 0 && foreignShipsPresent.size() == 0) {
          system.setOwner(orderedShip.owner);
       }
    }
